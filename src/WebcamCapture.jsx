d3904e32e97b0419cdd8fe9bff5777164682e40d
Yejune Ko <koyejune0302@gmail.com>
2024-08-18 06:37:08 +0900

    qwer

--- src/WebcamCapture.jsx
+++ src/WebcamCapture.jsx
@@ -44,8 +44,61 @@ const WebcamCapture = () => {
     });
   };
 
-  const goToNextStage = () => {
-    navigate('/game');
+  const goToNextStage = async () => {
+    const formData = new FormData();
+
+    // Upscale the image before sending it to the backend
+    upscaleImage(canvasRef.current, 900, 900) // Example: upscale to 800x600
+      .then(async (upscaledBlob) => {
+        if (upscaledBlob) {
+          formData.append('file', upscaledBlob, 'upscaled-image.png');
+
+        try {
+          // Send POST request to the backend server using Axios
+          const response = await axios.post('https://19cd-218-146-20-61.ngrok-free.app/start_game2', formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          });
+
+          const data = response.data;
+          console.log('Response:', data);
+          const userId = data.user_id;
+          console.log('userId:', userId);
+            const baseImage = data.bald_image;
+
+          if (response.status === 200) {
+            console.log('Image successfully sent to the server');
+            // Navigate to the /game page with state
+              navigate('/game', { state: { userId: `${userId}`, baseImage: `${baseImage}` } });
+          } else {
+            console.error('Failed to upload image');
+          }
+        } catch (error) {
+          console.error('Error uploading image:', error);
+        }
+      } else {
+          console.error('Failed to create upscaled Blob');
+        }
+      });
+  };
+
+  // Function to upscale the image using a new canvas
+  const upscaleImage = (canvas, targetWidth, targetHeight) => {
+    return new Promise((resolve) => {
+      const upscaledCanvas = document.createElement('canvas');
+      upscaledCanvas.width = targetWidth;
+      upscaledCanvas.height = targetHeight;
+      const context = upscaledCanvas.getContext('2d');
+
+      // Draw the original image onto the upscaled canvas with the new dimensions
+      context.drawImage(canvas, 0, 0, targetWidth, targetHeight);
+
+      // Convert the upscaled canvas to Blob
+      upscaledCanvas.toBlob((blob) => {
+        resolve(blob);
+    }, 'image/png');
+    });
   };
 
   return (
