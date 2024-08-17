b2537ef1defa6bb63be457cb824551fe33b4ec25
ChangHyunLee <dlckdgus200011@gmail.com>
2024-08-18 06:14:33 +0900

    finish page

--- src/FinishPage.jsx
+++ src/FinishPage.jsx
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import './FinishPage.css';
+import hanImage from './assets/han.jpeg';
 
 const FinishPage = () => {
     const [filterImage, setFilterImage] = useState(null);
+    const [showFlash, setShowFlash] = useState(false);
+    const [showFinish, setShowFinish] = useState(false);
     const location = useLocation();
 
     useEffect(() => {
-        // Retrieve the final image URL passed from the GamePage
-        const finalImage = location.state.finalImage;
-        console.log('Final Image:', finalImage);
+        const finalImage = location.state?.finalImage || hanImage;
         setFilterImage(finalImage);
-    }, [location.state.finalImage]);
+
+        const flashTimer = setTimeout(() => {
+            setShowFlash(true);
+        }, 3000); // 3초 후 섬광 효과 표시
+
+        const finishTimer = setTimeout(() => {
+            setShowFinish(true);
+        }, 3000); // 3.5초 후 "Finish" 텍스트 표시
+
+        return () => {
+            clearTimeout(flashTimer);
+            clearTimeout(finishTimer);
+        };
+    }, [location.state]);
 
     return (
         <div className="wrapper">
@@ -22,7 +36,9 @@ const FinishPage = () => {
                 {filterImage && (
                     <img src={filterImage} alt="Final Filtered Image" className="final-image" />
                 )}
+                {showFlash && <div className="flash-effect"></div>}
             </div>
+            {showFinish && <div className="finish-text">Finish</div>}
         </div>
     );
 };
