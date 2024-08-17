c0fe093fec4d816a50a6aafc3296ab6a6b9bb95c
Yejune Ko <koyejune0302@gmail.com>
2024-08-18 05:27:49 +0900

    asd

--- src/MainScreen.jsx
+++ src/MainScreen.jsx
@@ -7,7 +7,7 @@ const MainScreen = () => {
     <div className="main-screen">
       <h1 className="title">대머리 잔디 키우기 게임</h1>
       <p className="sub-title">대머리를 키워보세요!</p>
-      <img className="game-image" src="src/assets/growgrow.jpg" alt="grass" />\
+      <img className="game-image" src="src/assets/growgrow.jpg" alt="grass" />
 
       <Link to="/screenshot">
         <button className="main-button">Game Start</button>
